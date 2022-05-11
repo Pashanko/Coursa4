d0578d2dab00b00d31e389590fcbb1f34b77fddf
Pashanko <pnenchyn@ukr.net>
2022-05-11 22:44:58 +0300

    Initial commit

---
+++ src/main/java/ua/pashanko/coursach/CoursachApplication.java
@@ -0,0 +1,13 @@
+package ua.pashanko.coursach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursachApplication.class, args);
+    }
+
+}
