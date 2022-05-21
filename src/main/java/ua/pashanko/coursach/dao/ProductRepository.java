aa23e73c8821b16928c958fc3d755b3598b50785
Pashanko <pnenchyn@gmail.com>
2022-05-22 02:17:58 +0300

    Version 1.0.1 Added Simple Frontend to display dummy products

--- src/main/java/ua/pashanko/coursach/dao/ProductRepository.java
+++ src/main/java/ua/pashanko/coursach/dao/ProductRepository.java
@@ -1,7 +1,9 @@
 package ua.pashanko.coursach.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import ua.pashanko.coursach.entity.Product;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
